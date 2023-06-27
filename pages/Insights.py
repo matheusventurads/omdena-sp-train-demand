c53fb96f5b381ea0e8d94d7d7d661fe8fae91700
Matheus Ventura <107212644+matheusventurads@users.noreply.github.com>
2023-06-27 16:54:38 -0300

    Update Insights.py

--- pages/Insights.py
+++ pages/Insights.py
@@ -72,8 +72,9 @@ def run():
         of Brazil and the PTL (Passengers Transported) of Sao Paulo and found out high correlation between them .If we 
         compare PTL with the GDP of 1 month prior, the correlation between GDP and PTL reaches 0.8 .Hence using 1 
         month past GDP can be a very good indicator of the upcoming month's PTL trends. 
-    
-run()
+
       """)
 
     return None
+
+run()
